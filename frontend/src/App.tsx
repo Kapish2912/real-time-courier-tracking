f037af8845fbe7ede38c0e8d92c2be2463bfa809
Kapish2912 <onlinewithkapi@gmail.com>
2025-04-25 08:14:13 +0530

    Initial commit with frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-amber-300 text-5xl">Hello</h1>
+    <Button variant={"outline"}>This is beautiful</Button>
+    </>
+  )
+}
+
+export default App
